1635de64dbdd44f351fd87c0147b604c3d2218f3
MilkyasEyob.Habte <milkyas23>
2020-01-17 11:20:42 +0100

    ord lista på G

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,14 +1,128 @@
-import java.util.*;
+import java.util.ArrayList;
 
 public class Hangman {
     public static void main(String[] args) {
 
-        List<String> ls = new LinkedList<String>();
+        ArrayList<String> ordList = new ArrayList<String>();
 
 
-    }
+
+        ordList.add("vibe");          // At index 0
+        ordList.add("awkward");       // At index 1
+        ordList.add("quiz");          // At index 2
+        ordList.add("wave");          // At index 3
+        ordList.add("zombie");        // At index 4
+        ordList.add("youthful");      // At index 5
+        ordList.add("wizard");        // At index 6
+        ordList.add("boomer");        // At index 7
+        ordList.add("Java");          // At index 8
+        ordList.add("inteliJ");       // At index 9
+        ordList.add("Subway");        // At index 10
+        ordList.add("uptown");        // At index 11
+        ordList.add("staff");         // At index 12
+        ordList.add("strength");      // At index 13
+        ordList.add("puppy");         // At index 14
+        ordList.add("transgress");    // At index 15
+        ordList.add("transcript");    // At index 16
+        ordList.add("vodka");         // At index 17
+        ordList.add("walkway");       // At index 19
+        ordList.add("yummy");         // At index 20
+        ordList.add("unknown");             // At index 21
+        ordList.add("zipper");              // At index 22
+        ordList.add("whiskey");             // At index 23
+        ordList.add("unworthy");            // At index 24
+        ordList.add("transplant");          // At index 25
+        ordList.add("swivel");              // At index 26
+        ordList.add("snazzy");              // At index 27
+        ordList.add("scratch");             // At index 28
+        ordList.add("oxygen");              // At index 29
+        ordList.add("sphinx");              // At index 30
+        ordList.add("ovary");               // At index 31
+        ordList.add("stronghold");          // At index 32
+        ordList.add("stronghold");          // At index 33
+        ordList.add("luxury");              // At index 34
+        ordList.add("lymph");               // At index 35
+        ordList.add("lengths");             // At index 36
+        ordList.add("kiwifruit");           // At index 37
+        ordList.add("klutz");               // At index 38
+        ordList.add("knapsack");            // At index 39
+        ordList.add("larynx");              // At index 40
+        ordList.add("kitsch");              // At index 41
+        ordList.add("kiosk");               // At index 42
+        ordList.add("kilobyte");            // At index 43
+        ordList.add("khaki");               // At index 44
+        ordList.add("jumbo");               // At index 45
+        ordList.add("joyful");              // At index 46
+        ordList.add("juicy");               // At index 47
+        ordList.add("joking");              // At index 48
+        ordList.add("jockey");              // At index 49
+        ordList.add("jukebox");             // At index 50
+        ordList.add("jiujitsu");            // At index 50
+        ordList.add("jazzy");               // At index 51
+        ordList.add("jelly");               // At index 52
+        ordList.add("jigsaw");              // At index 53
+        ordList.add("jinx");                // At index 54
+        ordList.add("jazziest");            // At index 55
+        ordList.add("jaywalk");             // At index 56
+        ordList.add("jawbreaker");          // At index 57
+        ordList.add("jaundice");            // At index 58
+        ordList.add("injury");              // At index 59
+        ordList.add("ivory");               // At index 60
+        ordList.add("ivy");                 // At index 61
+        ordList.add("jackpot");             // At index 62
+        ordList.add("icebox");              // At index 63
+        ordList.add("iatrogenic");          // At index 64
+        ordList.add("hyphen");              // At index 65
+        ordList.add("haphazard");           // At index 66
+        ordList.add("gnostic");             // At index 67
+        ordList.add("gossip");              // At index 68
+        ordList.add("grogginess");          // At index 69
+        ordList.add("haiku");               // At index 70
+        ordList.add("gnarly");              // At index 71
+        ordList.add("glyph");               // At index 72
+        ordList.add("glowworm");            // At index 73
+        ordList.add("gizmo");               // At index 74
+        ordList.add("galaxy");              // At index 75
+        ordList.add("galvanize");           // At index 76
+        ordList.add("gazebo");              // At index 77
+        ordList.add("giaour");              // At index 78
+        ordList.add("gabby");               // At index 79
+        ordList.add("funny");               // At index 80
+        ordList.add("fuchsia");             // At index 81
+        ordList.add("frizzled");            // At index 82
+        ordList.add("fuchsia");             // At index 83
+        ordList.add("frizzled");            // At index 84
+        ordList.add("fluffiness");          // At index 85
+        ordList.add("flyby");               // At index 86
+        ordList.add("foxglove");            // At index 87
+        ordList.add("frazzled");            // At index 88
+        ordList.add("flopping");            // At index 89
+        ordList.add("flapjack");            // At index 90
+        ordList.add("fjord");               // At index 91
+        ordList.add("fixable");             // At index 92
+        ordList.add("euouae");              // At index 93
+        ordList.add("exodus");              // At index 94
+        ordList.add("faking");              // At index 95
+        ordList.add("fishhook");            // At index 96
+        ordList.add("espionage");           // At index 97
+        ordList.add("equip");               // At index 98
+        ordList.add("embezzle");            // At index 99
+        ordList.add("cycle");               // At index 100
+        ordList.add("curacao");             // At index 101
+        ordList.add("instagram");           // At index 102
+
+        ordList.add("");          // At index 50
+
+
+
+
+
+
+
+
 
 
 
     }
 }
+
